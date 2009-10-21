dde171205513258b406642d84892f782db2f8851
Pavel Gabriel <alovak@gmail.com>
2009-10-21 17:10:49 +0300

    MPI::Response (for Verification req) was added

--- lib/mpi_client/request/verification.rb
+++ lib/mpi_client/request/verification.rb
@@ -2,6 +2,34 @@ module MPI
   mattr_accessor :server_url
   self.server_url = 'http://mpi.server.com/'
 
+  class Response
+    attr_reader :xml, :error_code, :error_message, :status, :url
+
+    def initialize(xml)
+      @xml = xml
+    end
+
+    def successful?
+      !(error_message || error_code)
+    end
+
+    def parse
+      doc = Nokogiri::XML(xml)
+
+      unless (error = doc.xpath("//Error")).empty?
+        @error_message = error.text
+        @error_code    = error.attr('code')
+      else
+        @status = doc.xpath("//Transaction").attr('status')
+        @url = doc.xpath("//Transaction/URL").text
+      end
+    end
+
+    def self.parse(xml)
+      response = self.new(xml)
+    end
+  end
+
   class VerificationRequest
     PARAMS_MAP = {
       'AccountId'       => :account_id,
@@ -24,7 +52,7 @@ module MPI
     end
 
     def process
-      MPIResponse.parse(post(build_xml))
+      MPI::Response.parse(post(build_xml))
     end
 
     private
--- spec/request/verification_spec.rb
+++ spec/request/verification_spec.rb
@@ -1,6 +1,66 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe "VerificationRequest" do
+describe "Response" do
+  describe "parse method" do
+    it "should return Response instance" do
+      MPI::Response.parse('').should be_an_instance_of(MPI::Response)
+    end
+  end
+
+  context 'xml contain sucessful response' do
+    before(:each) do
+      @response = MPI::Response.new('')
+      @response.stub!(:xml => <<-XML)
+        <?xml version="1.0" encoding="UTF-8"?>
+        <Response type="vereq">
+            <Transaction id="33557" status="Y">
+                <URL>http://3ds/client/xxx</URL>
+            </Transaction>
+        </Response>
+        XML
+      @response.parse
+    end
+
+    it "should be successful if xml does not contain Error item" do
+      @response.should be_successful
+    end
+
+    it "should have 'Y' status" do
+      @response.status.should == 'Y'
+    end
+
+    it "should have url" do
+      @response.url.should == 'http://3ds/client/xxx'
+    end
+  end
+
+  context "xml contains Error node" do
+    before(:each) do
+      @response = MPI::Response.new('')
+      @response.stub!(:xml => <<-XML)
+        <?xml version="1.0" encoding="UTF-8"?>
+        <Response type="create_account">
+          <Error code="C5">Wrong data</Error>
+        </Response>
+        XML
+      @response.parse
+    end
+
+    it "should not be successful if xml contains Error item" do
+      @response.should_not be_successful
+    end
+
+    it "should contain error message" do
+      @response.error_message.should match /Wrong data/
+    end
+
+    it "should contain error code" do
+      @response.error_code.should == 'C5'
+    end
+  end
+end
+
+describe "Request" do
   before(:each) do
     options = {
       :account_id       => '100',
@@ -23,14 +83,14 @@ describe "VerificationRequest" do
     it "should return MPIResponse" do
       @request.should_receive(:build_xml).ordered
       @request.should_receive(:post).ordered
-      MPIResponse.should_receive(:parse)
+      MPI::Response.should_receive(:parse)
       @request.process
     end
 
     it "should invoke MPIResponse parse with xml response" do
       @request.stub!(:build_xml => 'builded xml')
       @request.should_receive(:post).with('builded xml').and_return('xml response')
-      MPIResponse.should_receive(:parse).with("xml response")
+      MPI::Response.should_receive(:parse).with("xml response")
       @request.process
     end
   end
