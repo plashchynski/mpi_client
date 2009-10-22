6270bf72e8554ae4d1c5f840801398d06fd4c164
Pavel Gabriel <alovak@gmail.com>
2009-10-22 15:52:01 +0300

    Small change

--- mpi_client.gemspec
+++ mpi_client.gemspec
@@ -7,7 +7,7 @@ Gem::Specification.new do |s|
   s.email        = 'plashchynski@gmail.com'
   s.require_path = "lib"
   s.has_rdoc     = false
-  s.files        =  ["lib/mpi_client/mpi_response.rb", "lib/mpi_client/option_translator.rb", "lib/mpi_client.rb", "remote_spec/mpi_client_spec.rb", "spec/mpi_client_spec.rb", "spec/mpi_response_spec.rb", "spec/option_translator_spec.rb"]
+  s.files        =  ["lib/mpi/option_translator.rb", "lib/mpi/response.rb", "lib/mpi/verification/request.rb", "lib/mpi/verification/response.rb", "lib/mpi/verification.rb", "lib/mpi.rb", "lib/mpi_client.rb"]
   s.rubygems_version = "1.3.0"
   s.add_dependency("nokogiri", [">= 1.3.2"])
   s.add_dependency("alovak-network", [">= 1.1.2"])
--- spec/remote/verification_spec.rb
+++ spec/remote/verification_spec.rb
@@ -5,14 +5,15 @@ MPI.server_url = 'http://192.168.65.11/xml'
 describe 'test of verification request' do
   it "should have status 'N'" do
     req = MPI::Verification::Request.new(request_params(:card_number => '4200000000000000'), '1')
-    result = req.process
-    result.status.should == 'N'
+    response = req.process
+    response.status.should == 'N'
   end
 
-  it "should return 'Y'" do
+  it "should return 'Y' and url" do
     req = MPI::Verification::Request.new(request_params(:card_number => '4012001037141112'), '2')
-    result = req.process
-    result.status.should == 'Y'
+    response = req.process
+    response.status.should == 'Y'
+    response.url.should_not be_empty
   end
 
   def request_params(new_params)
